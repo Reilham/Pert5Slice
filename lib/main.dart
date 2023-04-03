b5b52502c968e982229e867b65668428901699f1
Reilham RA <Reilham.ra@gmail.com>
2023-04-04 00:20:28 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pertemuan4slice/home_page.dart';
+import 'package:pertemuan4slice/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const homepage(),
+      home: mainpage(),
     );
   }
 }
